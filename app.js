5c6a3e578b5dd6196da48521aa054882c3b30011
Heemangi <rupaniheemangi5@gmail.com>
2024-05-02 17:54:19 +0530

    app

--- app.js
+++ app.js
@@ -9,21 +9,20 @@ import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
-
 const app = express();
 config();
 
-const corsOptions = {
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
     origin: "https://frontend-estate-app.vercel.app",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
-};
+}));
 
-app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
