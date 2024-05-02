d0fb35ae973b57e3977db7f488b81b484fb3f604
Heemangi <rupaniheemangi5@gmail.com>
2024-05-02 17:43:16 +0530

    changes in cors

--- app.js
+++ app.js
@@ -9,20 +9,21 @@ import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
+
 const app = express();
 config();
 
-// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(cors({
+const corsOptions = {
     origin: "https://frontend-estate-app.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
-}));
+};
 
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
@@ -30,6 +31,6 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
+app.listen(PORT || 8800, () => {
   console.log("Server is running!");
 });
